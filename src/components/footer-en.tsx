78077e373df406687eac07780838d25edb7d1c3e
rszia <b09902012@ntu.edu.tw>
2024-09-30 22:34:17 +0800

    fix: remove advisor on footer

--- src/components/footer-en.tsx
+++ src/components/footer-en.tsx
@@ -12,7 +12,6 @@ export default function Footer() {
               <strong>Email:</strong> ioltwtwiol@gmail.com<br/>
               <strong>Organizer:</strong> Graduate Institute of Linguistics, National Taiwan University<br/>
               <strong>Co-organizer:</strong> College of Liberal Arts, National Taiwan University<br/>
-              <strong>Advisor:</strong> K-12 Education Administration, Ministry of Education
             </p>
             <div className="social-links mt-3">
               <a href="https://www.facebook.com/Taiwan.Olympiad.in.Linguistics" className="facebook"><i className="bx bxl-facebook"></i></a>
@@ -57,7 +56,7 @@ export default function Footer() {
 
       <div className="container">
         <div className="copyright">
-          &copy; Taiwan Olympiad in Linguistics, 2024. Last Updated 2024/8/22
+          &copy; Taiwan Olympiad in Linguistics, 2024. Last Updated 2024/9/30
         </div>
         <div className="credits">
           {/* <!-- All the links in the footer should remain intact. -->
--- src/components/footer-zh.tsx
+++ src/components/footer-zh.tsx
@@ -12,7 +12,6 @@ export default function Footer() {
               <strong>Email:</strong> ioltwtwiol@gmail.com<br/>
               <strong>主辦單位:</strong> 國立台灣大學語言學研究所<br/>
               <strong>協辦單位:</strong> 國立台灣大學文學院<br/>
-              <strong>指導單位:</strong> 教育部國教署
             </p>
             <div className="social-links mt-3">
               <a href="https://www.facebook.com/Taiwan.Olympiad.in.Linguistics" className="facebook"><i className="bx bxl-facebook"></i></a>
@@ -57,7 +56,7 @@ export default function Footer() {
 
       <div className="container">
         <div className="copyright">
-          &copy; 台灣語言學奧林匹亞競賽, 2024. 最近更新日期：2024/8/22
+          &copy; 台灣語言學奧林匹亞競賽, 2024. 最近更新日期：2024/9/30
         </div>
         <div className="credits">
           {/* <!-- All the links in the footer should remain intact. -->
