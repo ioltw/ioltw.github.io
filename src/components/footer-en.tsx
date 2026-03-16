5f40641355fe00419fb1be6791e14dd79e4638f6
sokolychenlb <134065246+sokolychenlb@users.noreply.github.com>
2026-03-16 16:08:18 +0800

    Update footer-en.tsx

--- src/components/footer-en.tsx
+++ src/components/footer-en.tsx
@@ -57,7 +57,7 @@ export default function Footer() {
 
       <div className="container">
         <div className="copyright">
-          &copy; Taiwan Olympiad in Linguistics, 2024. Last Updated 2026/3/9
+          &copy; Taiwan Olympiad in Linguistics, 2024. Last Updated 2026/3/16
         </div>
         <div className="credits">
           {/* <!-- All the links in the footer should remain intact. -->
